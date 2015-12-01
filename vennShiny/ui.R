be673577c9a393dd4ddf02bef750cd0ef7e8018c
gunzapper <gunzapper@gmail.com>
2015-12-01 16:30:42 +0100

    Inserting the code for the new input

--- vennShiny/ui.R
+++ vennShiny/ui.R
@@ -18,11 +18,11 @@
        sidebarPanel(
          # Chose what kind of venn graph
          selectInput("venntype", "Choose a type of graph:",
-                     choices = c("simple", "proportional", "bvenn"))#,
+                     choices = c("simple", "proportional", "bvenn")),
          #tags$hr(),
 
          # Insert first file
-         #fileInput('file1', 'Choose a file', accept=excel.type),
+         fileInput('file1', 'Choose a file', accept=excel.type)#,
          #tags$hr()
        ),
        
